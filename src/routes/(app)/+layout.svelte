921e9b29fed42dbad2aff4137a94ef0961429197
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-03-21 17:19:48 +0100

    inital sveltekit with github login

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from '../$types.js';
+	import { enhance } from '$app/forms';
+
+	type Props = {
+		data: LayoutData;
+		children: Snippet;
+	};
+
+	let { data, children }: Props = $props();
+</script>
+
+<form method="POST" action="/?signout" use:enhance>
+	<button>Sign out</button>
+</form>
+
+{@render children()}
---
+++ src/routes/(app)/+page.server.ts
@@ -0,0 +1,15 @@
+import { fail, redirect } from '@sveltejs/kit';
+import { invalidateSession, deleteSessionTokenCookie } from '$lib/server/session';
+
+import type { Actions, PageServerLoad } from './$types';
+
+export const actions: Actions = {
+	signout: async (event) => {
+		if (event.locals.session === null) {
+			return fail(401);
+		}
+		await invalidateSession(event.locals.session.id);
+		deleteSessionTokenCookie(event);
+		return redirect(302, '/login');
+	}
+};
