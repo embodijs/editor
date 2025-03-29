dcb55c5133c47c24834a0b35d2aba73bfc125799
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-03-29 20:46:43 +0100

    use github rest api for getting orgs

--- src/server/services/org.ts
+++ src/server/services/org.ts
@@ -1,24 +1,11 @@
 import type { GitOrg } from '$core/model/org';
 import type { InternalGitUser } from '$core/model/user';
-import type { paths as GitHubPaths } from '../../github/interface';
-
-type GetOrgs = GitHubPaths['/user/orgs']['get']['responses']['200']['content']['application/json'];
+import { generateGithubBase, getGithubClient } from '$lib/server/git';
 
 export const getGithubOrgs = async (user: InternalGitUser): Promise<GitOrg[]> => {
-	const orgs = await fetch(`https://api.github.com/user/orgs`, {
-		headers: {
-			Accept: 'application/vnd.github.v3+json',
-			Authorization: `Bearer ${user.token}`,
-			'X-GitHub-Api-Version': '2022-11-28'
-		}
-	});
-
-	if (!orgs.ok) {
-		throw new Error(`Failed to fetch orgs: ${orgs.status}`);
-	}
-
-	const orgsJson = (await orgs.json()) as GetOrgs;
-	return orgsJson.map((org) => ({
+	const github = getGithubClient();
+	const { data } = await github.rest.orgs.list(generateGithubBase(user));
+	return data.map((org) => ({
 		name: org.login,
 		avatar: org.avatar_url,
 		id: org.id.toString(),
