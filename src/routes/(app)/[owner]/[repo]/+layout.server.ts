b3b937424d77ebd37b7b11abacf4f23cc44aa896
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-04-03 23:11:32 +0200

    load config on project open

--- src/routes/(app)/[owner]/[repo]/+layout.server.ts
+++ src/routes/(app)/[owner]/[repo]/+layout.server.ts
@@ -1,55 +1,10 @@
 import type { LayoutServerLoad } from './$types';
-import { hasProject, isAuthorized } from '$/lib/server/guards';
-import {
-	extractCollectionsTitles,
-	extractEmbodiConfig,
-	isGitFile,
-	loadEmbodiConfig
-} from '$core/logic/config';
-import { createInternalGitUser } from '$core/logic/user';
-import { createBaseGitRepo } from '$core/logic/repo';
-import { getRepoContentFromGithub } from '$services/repo';
-import { error } from '@sveltejs/kit';
-import { updateSession } from '$services/session';
+import { assertProject } from '$/lib/server/guards';
+import { extractCollectionsTitles } from '$core/logic/config';
 
-export const load: LayoutServerLoad = async ({ locals, params }) => {
-	if (
-		hasProject(locals) &&
-		locals.session.activeProjectConfig.owner === params.owner &&
-		locals.session.activeProjectConfig.repo === params.repo
-	) {
-		const collections = extractCollectionsTitles(locals.session.activeProjectConfig);
-		return {
-			collections
-		};
-	}
-
-	isAuthorized(locals);
-	const { owner, repo } = params;
-	const gitUser = createInternalGitUser(locals);
-	const gitRepo = createBaseGitRepo(params);
-
-	const gitFile = await loadEmbodiConfig((path: string) =>
-		getRepoContentFromGithub(path, gitRepo, gitUser)
-	);
-
-	if (!isGitFile(gitFile)) {
-		error(403, 'Valid Config is missing in the repository');
-	}
-
-	const config = extractEmbodiConfig(gitFile);
-
-	const collections = extractCollectionsTitles(config);
-
-	await updateSession({
-		...locals.session,
-		activeProjectConfig: {
-			...config,
-			owner,
-			repo
-		}
-	});
-
+export const load: LayoutServerLoad = async ({ locals }) => {
+	assertProject(locals);
+	const collections = extractCollectionsTitles(locals.session.activeProjectConfig);
 	return {
 		collections
 	};
--- src/routes/(app)/projects/+page.server.ts
+++ src/routes/(app)/projects/+page.server.ts
@@ -1,6 +1,12 @@
 import { isAuthorized } from '$/lib/server/guards';
+import { extractEmbodiConfig, isGitFile, loadEmbodiConfig } from '$core/logic/config';
+import { createBaseGitRepo } from '$core/logic/repo';
+import { createInternalGitUser } from '$core/logic/user';
 import { getProjects } from '$services/project';
-import type { PageServerLoad } from './$types';
+import { getRepoContentFromGithub } from '$services/repo';
+import { error, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { updateSession } from '$services/session';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	isAuthorized(locals);
@@ -12,3 +18,44 @@ export const load: PageServerLoad = async ({ locals }) => {
 		projects
 	};
 };
+
+export const actions = {
+	open: async ({ request, locals }) => {
+		isAuthorized(locals);
+
+		const formData = await request.formData();
+		const owner = formData.get('owner')?.toString();
+		const repo = formData.get('repo')?.toString();
+
+		if (!owner || !repo) {
+			return { error: 'Invalid form data' };
+		}
+
+		const gitUser = createInternalGitUser(locals);
+		const gitRepo = createBaseGitRepo({
+			owner,
+			repo
+		});
+
+		const gitFile = await loadEmbodiConfig((path: string) =>
+			getRepoContentFromGithub(path, gitRepo, gitUser)
+		);
+
+		if (!isGitFile(gitFile)) {
+			error(403, 'Valid Config is missing in the repository');
+		}
+
+		const config = extractEmbodiConfig(gitFile);
+
+		await updateSession({
+			...locals.session,
+			activeProjectConfig: {
+				...config,
+				owner,
+				repo
+			}
+		});
+
+		redirect(302, `/${owner}/${repo}`);
+	}
+} satisfies Actions;
