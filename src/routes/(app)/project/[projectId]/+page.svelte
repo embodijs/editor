f314268de8fa7a0feb4e362583822b642f13a7d7
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-09-19 16:36:24 +0200

    Cleanup old

--- src/routes/(app)/project/[projectId]/+page.svelte
+++ src/routes/(app)/project/[projectId]/+page.svelte
@@ -9,17 +9,3 @@
 </script>
 
 <h1>Projects</h1>
-<a class="btn btn-primary" href="/projects/add">Add</a>
-{#each data.projects as project (project.id)}
-	<Card>
-		<CardTitle>{project.name}</CardTitle>
-		<p>{project.description}</p>
-		<CardActions>
-			<form action="?/open" method="POST" use:enhance>
-				<input type="hidden" name="owner" value={project.owner} />
-				<input type="hidden" name="repo" value={project.repo} />
-				<button class="btn btn-primary">Open</button>
-			</form></CardActions
-		>
-	</Card>
-{/each}
