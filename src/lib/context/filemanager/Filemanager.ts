1485d46e9ee47ed40b8d8f6660f131c40248d8e2
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-10-10 01:25:55 +0200

    add hash to filename to avoid duplicates

--- src/lib/context/filemanager/Filemanager.ts
+++ src/lib/context/filemanager/Filemanager.ts
@@ -11,13 +11,38 @@ export class FileManager {
 		this.store = new Map();
 	}
 
-	set(file: File): string {
+	protected async hash(file: File) {
+		const buffer = await file.arrayBuffer();
+		const hashBuffer = await crypto.subtle.digest('SHA-256', buffer);
+		const hashArray = Array.from(new Uint8Array(hashBuffer));
+		const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+
+		return hashHex.substring(0, length);
+	}
+
+	protected addHashToFileName(name: string, hash: string): string {
+		const lastDot = name.lastIndexOf('.');
+
+		if (lastDot === -1) {
+			// Keine Extension (z.B. "README")
+			return `${name}-${hash}`;
+		}
+
+		const baseName = name.substring(0, lastDot);
+		const extension = name.substring(lastDot); // inkl. "."
+
+		return `${baseName}-${hash}${extension}`;
+	}
+
+	async set(file: File): Promise<string> {
 		if (this.store.has(file.name)) {
 			return this.store.get(file.name)!.relativePath;
 		}
+		const hash = await this.hash(file);
+		const fileName = this.addHashToFileName(file.name, hash);
 		const upload: FileUpload = {
-			relativePath: `./${file.name}`,
-			absolutePath: `${this.gitCurrentDirPath}/${file.name}`,
+			relativePath: `./${fileName}`,
+			absolutePath: `${this.gitCurrentDirPath}/${fileName}`,
 			blob: URL.createObjectURL(file)
 		};
 		this.store.set(upload.relativePath, upload);
