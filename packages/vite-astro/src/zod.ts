1df309a5b7a2b91e40c7a6831b090db536519156
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-10-15 01:43:45 +0200

    transform zod schema

--- packages/vite-astro/src/zod.ts
+++ packages/vite-astro/src/zod.ts
@@ -1,95 +1,208 @@
 import { z } from "zod";
-import type {
-  MetaInputField,
-  StringField,
-  NumberField,
-  DateField,
-  BooleanField,
-  ImageField,
-  FileField,
-  EnumField,
-} from "@emdodi/cms";
-import { optional } from "astro/zod";
+import type * as collection from "@embodi/cms";
 
-
-function getTypeDef<T extends z.core.$ZodType>(schema: T) {
+function getTypeDef<T extends z.core.$ZodType>(schema: T): T["_zod"]["def"] {
   return schema._zod.def;
-};
+}
 
-type Transformer = (schema: z.core.$ZodType, fieldName: string) => MetaInputField | null;
+type TypeTransformer<
+  O extends collection.MetaInputField = collection.MetaInputField,
+> = (schema: z.core.$ZodType, fieldName: string) => O | null;
 
-const prepareTransformers = (list: Transformer[], schema: z.core.$ZodType, fieldName: string): MetaInputField => {
+const prepareTransformers = (
+  list: TypeTransformer[],
+  schema: z.core.$ZodType,
+  fieldName: string,
+): ReturnType<TypeTransformer> => {
   for (const transformer of list) {
     const result = transformer(schema, fieldName);
     if (result) return result;
   }
+  return null;
 };
 
-const runTypeTranfromer = (schema: z.core.$ZodType, fieldName: string) => prepareTransformers([parseString, parseNumber], schema, fieldName)
+const runTypeTransformer = (schema: z.core.$ZodType, fieldName: string) =>
+  prepareTransformers(
+    [
+      parseImage, // need to be called before string
+      parseString,
+      parseNumber,
+      parseOptional,
+      parseObject,
+      parseArray,
+      parseBoolean,
+      parseDate,
+      parseEnum,
+    ], //order matters
+    schema,
+    fieldName,
+  );
 
-export const parseString: Transformer = (schema, fieldName) => {
-  const def = getTypeDef(schema) as z.core.$ZodStringDef;
-  if(def.type !== "string") {
+export const parseZodSchema = (schema: z.ZodObject) => {
+  const def = getTypeDef(schema);
+  if (def.type !== "object") {
     return null;
   }
-  const checks: [string, number | string][] | undefined =  def.checks?.map((check): [string, number | string] | null => {
-    const {def} = check._zod
-    if(def.check === 'string_format') {
-      return ['pattern', (def as z.core.$ZodCheckStringFormatDef).format];
-    } else if(def.check === 'min_length') {
-      return ['minLength', (def as z.core.$ZodCheckMinLengthDef).minimum];
-    } else if(def.check === 'max_length') {
-      return ['maxLength', (def as z.core.$ZodCheckMaxLengthDef).maximum];
-    }
+  const { shape } = def;
+  return Object.entries(shape)
+    .map(([key, value]) => runTypeTransformer(value, key))
+    .filter((v) => v != null);
+};
+
+const handleChecks = (def: z.core.$ZodTypeDef) => {
+  const checks: [string, number | string][] | undefined = def.checks
+    ?.map((check): [string, number | string] | null => {
+      const { def } = check._zod;
+      if (def.check === "string_format") {
+        return ["pattern", (def as z.core.$ZodCheckStringFormatDef).format];
+      } else if (def.check === "min_length") {
+        return ["minLength", (def as z.core.$ZodCheckMinLengthDef).minimum];
+      } else if (def.check === "max_length") {
+        return ["maxLength", (def as z.core.$ZodCheckMaxLengthDef).maximum];
+      } else if (def.check === "greater_than") {
+        return ["min", Number((def as z.core.$ZodCheckGreaterThanDef).value)];
+      } else if (def.check === "less_than") {
+        return ["max", Number((def as z.core.$ZodCheckLessThanDef).value)];
+      }
+      return null;
+    })
+    .filter((value) => value != null);
+  return checks ? Object.fromEntries(checks) : {};
+};
+
+export const parseString: TypeTransformer<collection.StringField> = (
+  schema,
+  fieldName,
+) => {
+  const def = getTypeDef(schema);
+  if (def.type !== "string") {
     return null;
-  }).filter((value) => value != null);
+  }
+
   return {
     fieldName,
     type: "string",
-    ...(checks ? Object.fromEntries(checks) : {})
+    ...handleChecks(def),
   };
-}
+};
 
-const parseNumber: Transformer = (schema) => {
+export const parseNumber: TypeTransformer<collection.NumberField> = (
+  schema,
+  fieldName,
+) => {
   const def = getTypeDef(schema);
-  if(def.type === "number") {
-    return {
-      name: def.name,
-      type: "number",
-    };
+  if (def.type !== "number") {
+    return null;
   }
-  return null;
-}
+  return {
+    fieldName,
+    type: "number",
+    ...handleChecks(def),
+  };
+};
 
-const parseObject: Transformer = (schema) => {
+const parseBoolean: TypeTransformer<collection.BooleanField> = (
+  schema,
+  fieldName,
+) => {
   const def = getTypeDef(schema);
-  if(def.type === "object") {
-    return {
-      name: def.name,
-      type: "object",
-    };
+  if (def.type !== "boolean") {
+    return null;
   }
-  return null;
-}
+  return {
+    fieldName,
+    type: "boolean",
+  };
+};
 
-export const parseOptional: Transformer = (schema, fieldName) => {
+const parseImage: TypeTransformer<collection.ImageField> = (
+  schema,
+  fieldName,
+) => {
+  const def = getTypeDef(schema);
+  const meta = (schema as z.ZodString).meta();
+  if (def.type !== "string" || meta?.type !== "image") {
+    return null;
+  }
+  return {
+    fieldName,
+    type: "image",
+    ...handleChecks(def),
+  };
+};
+
+export const parseArray: TypeTransformer = (schema, fieldName) => {
+  const def = getTypeDef(schema) as z.core.$ZodArrayDef;
+  if (def.type !== "array") {
+    return null;
+  }
+  const sub = runTypeTransformer(def.element, "items");
+  if (!sub) {
+    return null;
+  }
+  return {
+    fieldName,
+    type: "array",
+    items: sub,
+  };
+};
+
+export const parseDate: TypeTransformer = (schema, fieldName) => {
+  const def = getTypeDef(schema);
+  if (def.type !== "date") {
+    return null;
+  }
+  return {
+    fieldName,
+    type: "date",
+    ...handleChecks(def),
+  };
+};
+
+export const parseEnum: TypeTransformer<collection.EnumField> = (
+  schema,
+  fieldName,
+) => {
+  const def = getTypeDef(schema);
+  if (def.type !== "enum") {
+    return null;
+  }
+  const { entries } = def as z.core.$ZodEnumDef;
+  console.log({ entries });
+  return {
+    fieldName,
+    type: "enum",
+    options: entries,
+    ...handleChecks(def),
+  };
+};
+
+export const parseOptional: TypeTransformer = (schema, fieldName) => {
   const def = getTypeDef(schema) as z.core.$ZodOptionalDef;
-  if(def.type === "optional") {
+  if (def.type === "optional") {
+    const sub = runTypeTransformer(def.innerType, fieldName);
+    if (!sub) {
+      return null;
+    }
     return {
-      ...runTypeTranfromer(def.innerType, fieldName),
+      ...sub,
       optional: true,
     };
   }
   return null;
-}
+};
 
-const parseNullable: Transformer = (schema) => {
+export const parseObject: TypeTransformer = (schema, fieldName) => {
   const def = getTypeDef(schema);
-  if(def.type === "nullable") {
-    return {
-      name: def.name,
-      type: "nullable",
-    };
+  if (def.type !== "object") {
+    return null;
   }
-  return null;
-}
+  const { shape } = def as z.core.$ZodObjectDef;
+  return {
+    fieldName,
+    type: "object",
+    fields: Object.entries(shape)
+      .map(([key, value]) => runTypeTransformer(value, key))
+      .filter((v) => v !== null),
+  };
+};
