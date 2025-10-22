023abd20ee4a5de062c9d50961aefea0e912c858
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-10-22 23:20:17 +0200

    fix login cookie

--- packages/embodi-cms-ui/src/server/services/oauth.ts
+++ packages/embodi-cms-ui/src/server/services/oauth.ts
@@ -5,15 +5,16 @@ export const github = new GitHub(env.GITHUB_CLIENT_ID, env.GITHUB_CLIENT_SECRET,
 
 const OAUTH_STATE_KEY = 'github_oauth_state';
 
-export const setOauthStateCookie = async (event: RequestEvent, state: string) => {
-	event.cookies.set(OAUTH_STATE_KEY, state, {
-		path: '/',
+export const setOauthStateCookie = (event: RequestEvent, state: string) => {
+	event.cookies.set('github_oauth_state', state, {
 		httpOnly: true,
-		maxAge: 60 * 60,
-		sameSite: 'strict'
+		maxAge: 60 * 10,
+		secure: import.meta.env.PROD,
+		path: '/',
+		sameSite: 'lax'
 	});
 };
 
-export const getOauthStateCookie = async (event: RequestEvent) => {
+export const getOauthStateCookie = (event: RequestEvent) => {
 	return event.cookies.get(OAUTH_STATE_KEY) ?? null;
 };
